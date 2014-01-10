1923e13add0e75f17059ec0d3b08f55cd13d33b7
Patrick Steele-Idem <psteeleidem@ebay.com>
2014-01-10 06:55:51 -0700

    Updated transport exports

--- transport/lib/index.js
+++ transport/lib/index.js
@@ -1,22 +1,10 @@
-function runCode(logicalPath, streamOrString) {
-
-}
-
-function registerDependencyCode(logicalParentPath, targetId, targetVersion) {
-
-}
-
-function registerMainCode(realPath, main) {
-
-}
-
 function resolveRequire(path, from) {
 
 }
 
 exports.defineCode = require('./defineCode');
-exports.runCode = runCode;
-exports.registerDependencyCode = registerDependencyCode;
-exports.registerMainCode = registerMainCode;
+exports.runCode = require('./runCode');
+exports.registerDependencyCode = require('./registerDependencyCode');
+exports.registerMainCode = require('./registerMainCode');
 exports.getPathInfo = require('./getPathInfo');
 exports.resolveRequire = resolveRequire;
