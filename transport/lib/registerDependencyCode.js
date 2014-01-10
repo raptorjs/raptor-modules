579dd3f2e988441622de76041e7732478b382c0c
Patrick Steele-Idem <psteeleidem@ebay.com>
2014-01-10 07:02:23 -0700

    End the stream

--- transport/lib/registerDependencyCode.js
+++ transport/lib/registerDependencyCode.js
@@ -4,7 +4,9 @@ function registerDependencyCode(logicalParentPath, childId, childVersion) {
     var out = resumer();
     out.write('$rmod.dep(' + JSON.stringify(logicalParentPath) + ', ' +
         JSON.stringify(childId) + ', ' +
-        JSON.stringify(childVersion));
+        JSON.stringify(childVersion) + ');');
+
+    out.end();
 
     return out;
 }
