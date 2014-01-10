e0f40889555383143f5c9f81524fb098497fc126
Patrick Steele-Idem <psteeleidem@ebay.com>
2014-01-10 07:02:29 -0700

    End the stream

--- transport/lib/registerMainCode.js
+++ transport/lib/registerMainCode.js
@@ -4,6 +4,7 @@ function registerMainCode(realPath, main) {
     var out = resumer();
     out.write('$rmod.main(' + JSON.stringify(realPath) + ', ' +
         JSON.stringify(main));
+    out.end();
     return out;
 }
 
