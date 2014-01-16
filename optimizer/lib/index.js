9b0c6712d2453d9dcf00bd33f63d57dfaf247cfd
Patrick Steele-Idem <psteeleidem@ebay.com>
2014-01-15 17:17:05 -0700

    addNormalizer instead of registerNormalizer

--- optimizer/lib/index.js
+++ optimizer/lib/index.js
@@ -4,7 +4,7 @@ function registerDependencyTypes(optimizer) {
     optimizer.dependencies.registerPackageType('require', require('./Dependency_require'));
     optimizer.dependencies.registerJavaScriptType('commonjs-def', require('./Dependency_commonjs-def'));
     optimizer.dependencies.registerJavaScriptType('commonjs-run', require('./Dependency_commonjs-run'));
-    optimizer.dependencies.registerNormalizer(function(dependency) {
+    optimizer.dependencies.addNormalizer(function(dependency) {
         if (typeof dependency === 'string') {
             if (dependency.startsWith('require ')) {
                 return {
