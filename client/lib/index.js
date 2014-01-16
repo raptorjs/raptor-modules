d33ec750a556e50e28cf272536662dfb9a10f6fc
Phil Gates-Idem <phillip.idem@gmail.com>
2014-01-16 16:45:53 -0500

    Initial pass at client modules (working on resolve)

--- client/lib/index.js
+++ client/lib/index.js
@@ -2,64 +2,261 @@
 
 
 /*
+https://github.com/joyent/node/blob/master/lib/module.js
+
 GOAL: This module should exactly mirror the NodeJS module system.
-The module transport will generate code that provides definitions
-modules. This information is used to resolve dependencies on
-client-side (in the browser).
+The module transport will generate code that is used for resolving
+real paths for a given logical path. This information is used to
+resolve dependencies on client-side (in the browser).
 */
 
+// this object stores the module factories with the keys being real paths of module (e.g. "/baz@3.0.0/lib/index" --> Function)
+var definitions = {};
+
+// this object stores the Module instance cache with the keys being logical paths of modules (e.g., "/$/foo/$/baz" --> Module)
+var modules = {};
+
+// this object maps dependency logical path to a specific version (for example, "/$/foo/$/baz" --> "3.0.0")
+var dependencies = {};
+
+// temporary variable for referencing a prototype
+var proto;
+
+function Module() {
+    /*
+    A Node module has these properties:
+    - filename: The logical path of the module
+    - id: (same as filename)
+    - exports: The exports provided during instantiation
+    - parent: parent Module
+    - loaded: Has module been fully loaded (set to false until factory function returns)
+    - children: The modules that were required by this module
+    - paths: The search path used by this module (not documented in Node.js module system so we don't need support)
+    */
+}
+
+var proto = Module.prototype;
 /*
-File Modules#
-If the exact filename is not found, then node will attempt to load the required filename with the added extension of .js, .json, and then .node.
+proto.createInstance = function(logicalPath) {
+    
+    var factoryOrObject = this.factoryOrObject;
 
-.js files are interpreted as JavaScript text files, and .json files are parsed as JSON text files. .node files are interpreted as compiled addon modules loaded with dlopen.
+    // check to see if we need to call a factory function to get the module instance
+    if (!factoryOrObject || factoryOrObject.constructor !== Function) {
+        // factoryObject is definitely not a function so use the factory object as the instance
+        this.exports = exports = factoryOrObject;
+    } else {
 
-A module prefixed with '/' is an absolute path to the file. For example, require('/home/marco/foo.js') will load the file at /home/marco/foo.js.
+        // exports is an empty object
+        this.exports = exports = {};
 
-A module prefixed with './' is relative to the file calling require(). That is, circle.js must be in the same directory as foo.js for require('./circle') to find it.
+        // realPath is the __filename
+        var realPath = this.realPath;
 
-Without a leading '/' or './' to indicate a file, the module is either a "core module" or is loaded from a node_modules folder.
+        //find the last slash position so that we can get the directory name substring
+        var pos = realPath.lastIndexOf('/');
 
-If the given path does not exist, require() will throw an Error with its code property set to 'MODULE_NOT_FOUND'.
+        // the require function is scoped to this module instance
+        var require = function(logicalPath) {
 
-Loading from node_modules Folders#
-If the module identifier passed to require() is not a native module, and does not begin with '/', '../', or './', then node starts at the parent directory of the current module, and adds /node_modules, and attempts to load the module from that location.
+        };
 
-If it is not found there, then it moves to the parent directory, and so on, until the root of the tree is reached.
+        // NOTE: We are using the scope of this Module instance but I don't know if we should do that
+        factoryOrObject.call(this, require, exports, this, realPath, realPath.substring(0, pos));
 
-For example, if the file at '/home/ry/projects/foo.js' called require('bar.js'), then node would look in the following locations, in this order:
-
-/home/ry/projects/node_modules/bar.js
-/home/ry/node_modules/bar.js
-/home/node_modules/bar.js
-/node_modules/bar.js
-This allows programs to localize their dependencies, so that they do not clash.
-
+        // The factory function may have changed the exports property to a new value so make sure
+        // the local exports variable references the latest exports
+        exports = this.exports;
+    }
+};
 */
 
 /**
  * Defines a packages whose metadata is used by raptor-loader to load the package.
  */
-function defineModule(modulePath, factoryFunc) {
+function define(realPath, factoryOrObject) {
     /*
     $rmod.def('/baz@3.0.0/lib/index', function(require, exports, module, __filename, __dirname) {
         // module source code goes here
     });
     */
+
+    /* jshint boss:true */
+    return (definitions[realPath] = factoryOrObject);
 }
 
-/*
-In addition, the following code would be sent down to the browser
-in order to allow client code to add "symbolic links" to allow the
-logical paths to be maintained:
+function registerDependency(logicalParentPath, dependencyId, dependencyVersion) {
+    var logicalPath = logicalParentPath + '/$/' + dependencyId;
+    dependencies[logicalPath] =  dependencyVersion;
+}
 
-$rmod.dep('', 'foo', '1.0.0');          // /node_modules/foo → /foo@1.0.0
-$rmod.dep('', 'bar', '2.0.0');          // /node_modules/bar → /bar@2.0.0
-$rmod.dep('/node_modules/foo', 'baz', '3.0.0'); // /node_modules/foo/node_modules/baz → /baz@3.0.0
-$rmod.dep('/node_modules/bar', 'baz', '3.0.0'); // /node_modules/bar/node_modules/baz → /baz@3.0.0
-*/
-function registerModuleDependency(basePath, moduleName, moduleVersion) {
+function resolvedInfo(logicalPath, dependencyId, subpath, dependencyVersion) {
+    // Our internal module resolver will return an object with the following properties:
+    // - logicalPath: The logical path of the module (used for caching instances)
+    // - realPath: The real path of the module (used for instantiating new instances via factory)
+    return {
+        logicalPath: logicalPath + subpath,
+        realPath: '/' + dependencyId + '@' + dependencyVersion + subpath,
+    };
+}
 
+function normalizePathParts(parts) {
+    var i;
+    var len = 0;
+
+    var numParts = parts.length;
+    for (i = 0; i < numParts; i++) {
+        var part = parts[i];
+        // ignore parts with just "."
+        if (part !== '.') {
+            if (part === '..') {
+                // overwrite the previous item by decrementing length
+                len--;
+            } else {
+                // add this part to result and increment length
+                parts[len] = part;
+                len++;
+            }
+        }
+    }
+
+    // truncate parts to remove unused
+    parts.length = len;
+    return parts.join('/');
+}
+
+function normalize(path) {
+    return normalizePathParts(path.split('/'));
+}
+
+function join(first, second) {
+    var firstParts = first.split('/');
+    var secondParts = second.split('/');
+    return normalizePathParts(firstParts.concat(secondParts));
+}
+
+function resolveAbsolute(target, from) {
+    var start = target.lastIndexOf('$');
+    if (start === -1) {
+        // target is something like "/foo/baz"
+        return {
+            logicalPath: target,
+            realPath: target
+        };
+    }
+
+    // target is something like "/$/foo/$/baz/lib/index"
+    start += 2;
+    var end = target.indexOf('/', start + 3);
+    var logicalPath;
+    var subpath;
+    var dependencyId;
+
+    if (end === -1) {
+        // target is something like "/$/foo/$/baz"
+        logicalPath = target;
+        subpath = '';
+        dependencyId = target.substring(start);
+    } else {
+        // target is something like "/$/foo/$/baz/lib/index"
+        logicalPath = target.substring(0, end);
+        subpath = target.substring(end);
+        dependencyId = target.substring(start, end);
+    }
+
+    var dependencyVersion = dependencies[logicalPath];
+    if (dependencyVersion) {
+        return resolvedInfo(logicalPath, dependencyId, subpath, dependencyVersion);
+    }
+
+    return null;
+}
+
+function resolveRelative(target, from) {
+    return resolveAbsolute(join(from, target));
+}
+
+function resolve(target, from) {
+    
+    if (!target) {
+        return null;
+    }
+
+    if (target.charAt(0) === '.') {
+        return resolveRelative(target, from);
+    }
+
+    if (target.charAt(0) === '/') {
+        return resolveAbsolute(normalize(target), from);
+    }
+
+    var dependencyVersion;
+    var pos;
+    var dependencyId;
+    var subpath;
+
+    pos = target.indexOf('/');
+    if (pos === -1) {
+        dependencyId = target;
+        subpath = '';
+    } else {
+        // When we're resolving a module, we don't care about the subpath at first
+        dependencyId = target.substring(0, pos);
+        subpath = target.substring(pos);
+    }
+
+    /*
+    Consider when the module "baz" (which is required by "foo") requires module "async":
+    resolve('async', '/$/foo/$/baz');
+
+    // TRY
+    /$/foo/$/baz/$/async
+    /$/foo/$/async
+    /$/async
+
+    // SKIP
+    /$/foo/$/$/async
+    /$/$/async
+    */
+
+    // First check to see if there is a sibling "$" with the given target
+    // by adding "/$/<target>" to the given from path.
+    // If the given from is "/$/foo/$/baz" then we will try "/$/foo/$/baz/$/async"
+    var logicalPath = from + '/$/' + dependencyId;
+    dependencyVersion = dependencies[logicalPath];
+    if (dependencyVersion) {
+        return resolvedInfo(logicalPath, dependencyId, subpath, dependencyVersion);
+    }
+
+    var end = from.lastIndexOf('/');
+
+    // if there is no "/" in the from path then this path is technically invalid (right?)
+    if (end !== -1) {
+        do {
+            var start = from.lastIndexOf('/', end - 1);
+            if (start === -1) {
+                // reached the start so stop searching
+                break;
+            }
+
+            // check to see if the substring from start:end is '/$/'
+            if ((end - start === 2) && (from.charAt(start + 1) === '$')) {
+                // skip look at this subpath because it ends with "/$/"
+                end = start;
+                continue;
+            }
+
+            logicalPath = from.substring(0, end) + '/$/' + dependencyId;
+            dependencyVersion = dependencies[logicalPath];
+            if (dependencyVersion) {
+                return resolvedInfo(logicalPath, dependencyId, subpath, dependencyVersion);
+            }
+
+            end = from.lastIndexOf('/', start - 1);
+        } while(end > 1);
+    }
+
+    return null;
 }
 
 /*
@@ -67,43 +264,34 @@ $rmod.run('/src/ui-pages/login/login-page', function(require, exports, module, _
     // module source code goes here
 });
 */
-function registerRunModule(modulePath, factoryFunc) {
-
+function runModule(logicalPath, factory) {
+    
 }
 
 /*
-Node.js allows a main script to be associated with a directory.
-If the directory is required'd then the main script will be used
-to determine which module is actually resolved. The main script
-can be explicitly declared in a package.json file contained in
-the directory or, if not explicitly declared, a default main
-script of "index" will be assumed. The main script is assumed
-to be relative to the module directory.
-
-To allow for mapping a directory to a main script, the following code will be used:
-$rmod.main('/foo@1.0.0', 'lib/index');
-
-By registering a main script for the /foo@1.0.0 module, the following will work as expected:
-
-require.resolve('foo'); // Returns "/node_modules/foo/lib/index
-*/
-function registerMain() {
-
-}
-
+ * $rmod is the short-hand version that that the transport layer expects
+ * to be in the browser window object
+ */
 var $rmod = {
-    def: defineModule,
-
-    dep: registerModuleDependency,
-
-    run: registerRunModule,
-
-    main: registerMain
+    def: define,
+    dep: registerDependency,
+    run: runModule,
+    //main: registerMain
 };
 
-
 if (typeof window === 'undefined') {
-    module.exports = $rmod;
+    module.exports = {
+        // expose the $rmod for testing the interface that would normally be exposed via the browser window object
+        $rmod: $rmod,
+
+        // expose the methods that we implement
+        define: define,
+        registerDependency: registerDependency,
+        resolve: resolve,
+        runModule: runModule,
+        normalize: normalize,
+        join: join
+    };
 } else {
     window.$rmod = $rmod;
 }
--- client/test/test.js
+++ client/test/test.js
@@ -4,8 +4,107 @@ var expect = require('chai').expect;
 
 describe('raptor-modules/client' , function() {
 
-    it('should implement $rmod based on specification', function(done) {
-        expect(true).to.equal(true);
+    beforeEach(function(done) {
+        for (var k in require.cache) {
+            if (require.cache.hasOwnProperty(k)) {
+                delete require.cache[k];
+            }
+        }
+        done();
+    });
+
+    it('should resolve modules using search path', function(done) {
+        var clientImpl = require('../lib/index');
+
+        // define a module for a given real path
+        clientImpl.define('/baz@3.0.0/lib/index', function(require, exports, module, __filename, __dirname) {
+            module.exports.test = true;
+        });
+
+        // Module "foo" requires "baz" 3.0.0
+        // This will create the following link:
+        // /$/foo/$/baz --> baz@3.0.0
+        clientImpl.registerDependency('/$/foo', 'baz', '3.0.0');
+
+        var resolved;
+
+        // Make sure that if we try to resolve "baz/lib/index" from within some module
+        // located at "/$/foo" then we should get back "/$/foo/$/baz"
+        resolved = clientImpl.resolve('baz/lib/index', '/$/foo');
+        expect(resolved.logicalPath).to.equal('/$/foo/$/baz/lib/index');
+        expect(resolved.realPath).to.equal('/baz@3.0.0/lib/index');
+
+        // A module further nested under /$/foo should also resolve to the same
+        // logical path
+        resolved = clientImpl.resolve('baz/lib/index', '/$/foo/some/other/module');
+        expect(resolved.logicalPath).to.equal('/$/foo/$/baz/lib/index');
+        expect(resolved.realPath).to.equal('/baz@3.0.0/lib/index');
+
+        // Code at under "/some/module" doesn't know about baz
+        expect(clientImpl.resolve('baz/lib/index', '/some/module')).to.equal(null);
+
+        done();
+    });
+
+    it('should resolve absolute paths', function(done) {
+
+        var clientImpl = require('../lib/index');
+
+        var resolved;
+
+        // define a module for a given real path
+        clientImpl.define('/baz@3.0.0/lib/index', function(require, exports, module, __filename, __dirname) {
+            module.exports.test = true;
+        });
+
+        // Module "foo" requires "baz" 3.0.0
+        // This will create the following link:
+        // /$/foo/$/baz --> baz@3.0.0
+        clientImpl.registerDependency('/$/foo', 'baz', '3.0.0');
+
+        // Make sure that if we try to resolve "baz" from within some module
+        // located at "/$/foo" then we should get back "/$/foo/$/baz"
+        resolved = clientImpl.resolve(
+            '/$/foo/$/baz/lib/index' /* target is absolute path */,
+            '/$/foo' /* the from is ignored */);
+
+        expect(resolved.logicalPath).to.equal('/$/foo/$/baz/lib/index');
+        expect(resolved.realPath).to.equal('/baz@3.0.0/lib/index');
+
+        resolved = clientImpl.resolve(
+            '/baz@3.0.0/lib/index' /* target is absolute path to specific version of module */,
+            '/$/foo' /* from is ignored if target is absolute path */);
+
+        expect(resolved.logicalPath).to.equal('/baz@3.0.0/lib/index');
+        expect(resolved.realPath).to.equal('/baz@3.0.0/lib/index');
+
+        // A module further nested under /$/foo should also resolve to the same logical path
+        resolved = clientImpl.resolve('baz', '/$/foo/some/other/module');
+        expect(resolved.logicalPath).to.equal('/$/foo/$/baz');
+        expect(resolved.realPath).to.equal('/baz@3.0.0');
+
+        // Without registering "main", "/baz@3.0.0" will not be known
+        resolved = clientImpl.resolve('/baz@3.0.0', '/some/module');
+
+        expect(resolved.realPath).to.equal('/baz@3.0.0');
+        expect(resolved.logicalPath).to.equal('/baz@3.0.0');
+        
+        done();
+    });
+
+    it('should normalize paths', function(done) {
+        var clientImpl = require('../lib/index');
+        expect(clientImpl.normalize('abc')).to.equal('abc');
+        expect(clientImpl.normalize('./abc')).to.equal('abc');
+        expect(clientImpl.normalize('abc/./def')).to.equal('abc/def');
+        expect(clientImpl.normalize('abc/../def')).to.equal('def');
+        done();
+    });
+
+    it('should join paths', function(done) {
+        var clientImpl = require('../lib/index');
+        expect(clientImpl.join('/foo/baz', './abc.js')).to.equal('/foo/baz/abc.js');
+        expect(clientImpl.join('/foo/baz', '../abc.js')).to.equal('/foo/abc.js');
         done();
     });
 });
