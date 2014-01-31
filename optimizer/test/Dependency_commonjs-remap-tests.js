912722e2d3d0db53d36403489368164cdf47d550
Steele-Idem <psteeleidem@LM-SJC-00873943.corp.ebay.com>
2014-01-30 23:15:56 -0800

    Updated module tests

--- optimizer/test/Dependency_commonjs-remap-tests.js
+++ optimizer/test/Dependency_commonjs-remap-tests.js
@@ -9,7 +9,7 @@ var fs = require('fs');
 
 require('../'); // Load this module just to make sure it works
 
-describe('raptor-modules/optimizer/Dependency_commonjs-main' , function() {
+describe('raptor-modules/optimizer/Dependency_commonjs-remap' , function() {
 
     beforeEach(function(done) {
         for (var k in require.cache) {
@@ -20,18 +20,18 @@ describe('raptor-modules/optimizer/Dependency_commonjs-main' , function() {
         done();
     });
 
-    it('should generate the correct main for an installed module', function(done) {
+    it('should generate the correct remap code', function(done) {
 
-        var defDependency = require('../lib/Dependency_commonjs-main');
-        defDependency.dir = "/foo@1.0.0";
-        defDependency.main = "lib/index";
+        var defDependency = require('../lib/Dependency_commonjs-remap');
+        defDependency.from = "/foo@1.0.0/lib/index";
+        defDependency.to = "browser/index";
         var code = '';
         defDependency.read()
             .on('data', function(data) {
                 code += data;
             })
             .on('end', function() {
-                expect(code).to.equal('$rmod.main("/foo@1.0.0", "lib/index");');
+                expect(code).to.equal('$rmod.remap("/foo@1.0.0/lib/index", "browser/index");');
                 done();
             })
             .on('error', done);
