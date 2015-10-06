3bfb9f8b5587ac4ff5702fb870ac8b2108d1fd75
Patrick Steele-Idem <pnidem@gmail.com>
2015-10-06 17:31:13 -0600

    Fixes https://github.com/lasso-js/lasso-require/issues/13 - Remove $ appearing in filenames

--- util/lib/getPathInfo.js
+++ util/lib/getPathInfo.js
@@ -14,8 +14,18 @@ var sep = nodePath.sep;
 function normalizeDepDirnames(path) {
     var parts = path.split(/[\\/]/);
     for (var i=0, len=parts.length; i<len; i++) {
-        if (parts[i] === 'node_modules') {
+        var part = parts[i];
+
+        if (part === 'node_modules') {
             parts[i] = '$';
+        } else {
+            // Replacing "$" characters with a "!" is kind of a hack
+            // but it works well enough. We just can't allow "$" in the
+            // logical or real path since "$" has a special meaning
+            // as a "node_modules" directory. This fixes the following
+            // issue:
+            // Fixes https://github.com/lasso-js/lasso-require/issues/13
+            parts[i] = part.replace(/[$]/g, '!');
         }
     }
 
